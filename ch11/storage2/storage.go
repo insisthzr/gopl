5aa497682623f127cad727dab762ef49382b4112
Isaac <hzr_bee@163.com>
2016-12-05 20:00:20 +0800

    test

---
+++ ch11/storage2/storage.go
@@ -0,0 +1,37 @@
+package storage
+
+import (
+	"net/smtp"
+	"log"
+	"fmt"
+)
+
+func bytesInUse(username string) int64 {
+	return 980 * 1e6
+}
+
+const sender = "test@163.com"
+const password = "test"
+const hostname = "smtp.test.com"
+
+const template = `Warning: you are using %d bytes of storage,
+%d%% of your quota.`
+
+var notifyUser = func(username, msg string) {
+	auth := smtp.PlainAuth("", sender, password, hostname)
+	err := smtp.SendMail(hostname + ":587", auth, sender, []string{username}, []byte(msg))
+	if err != nil {
+		log.Printf("smtp.SendEmail(%s) failed: %s", username, err)
+	}
+}
+
+func CheckQuota(username string) {
+	used := bytesInUse(username)
+	const quota = 1e9
+	percent := 100 * used / quota
+	if percent < 90 {
+		return
+	}
+	msg := fmt.Sprintf(template, used, percent)
+	notifyUser(username, msg)
+}
